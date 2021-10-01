42327da4f60d3da4c44cc0586f8de717942c0fba
deepak2107 <71771685+deepak2107@users.noreply.github.com>
2021-10-01 17:59:11 +0530

    Create fizzbuzz4.cpp
    I added a new C++ FizzBuzz code.

---
+++ C++/fizzbuzz4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <iterator>
+#include <climits>
+
+using namespace std;
+
+void fizzbuzz(){
+    for (int i=0;i<=2021;i++){
+        if ((i&(i-1))==0)
+            cout<<"FizzBuzz "<<i<<"\n";
+    }
+}
+
+int main(){
+    fizzbuzz();
+    return 0;
+}
