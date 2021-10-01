07ffcae10ef0e9bbcc6568909a2e37abe0455a2c
Ashwanigupta9125 <ashwanigameup@gmail.com>
2021-10-01 21:14:54 +0530

    one of code in c++ file is change because of some correction

--- C++/fizzbuzz_easy _understand2.cpp
+++ C++/fizzbuzz_easy _understand2.cpp
@@ -1,15 +1,14 @@
-class Solution {
-    public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<String>(n);
+
+        vector<string> fizzBuzz(int n) {
+        vector<string> answer; 
         if(n == 0) return answer;
         int k = 1;
         while(k!=n+1){
-            if(k%15 == 0) answer.add("FizzBuzz");
-            else if(k%3 == 0) answer.add("Fizz");
-            else if(k%5 == 0) answer.add("Buzz");
-            else answer.add(String.valueOf(k));
+            if(k%15 == 0) answer.push_back("FizzBuzz");
+            else if(k%3 == 0) answer.push_back("Fizz");
+            else if(k%5 == 0) answer.push_back("Buzz");
+            else answer.push_back(to_string(k));
             k++;
         }
         return answer;
     }
-}
