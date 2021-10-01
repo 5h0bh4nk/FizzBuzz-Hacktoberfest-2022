4a9e8c2c2ed62f0884f26b973f1c1ded3ca5fbb7
Siddharth1002 <soma_841975@student.nitw.ac.in>
2021-10-01 09:54:16 +0000

    Added 2 new solutions of Fizzbuzz in C++

--- C++/fizzbuzz3.cpp
+++ C++/fizzbuzz3.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 
 int main(){
-    int cnt3 = 0 , cnt5 = 0;
-    string str = "";
     for(int i = 1 ; i <= 100 ; i++){
-        cnt3++;
-        cnt5++;
-        str = "";
-        if(cnt3 == 3) { str += "Fizz"; cnt3 = 0; }
-        if(cnt5 == 5) { str += "Buzz"; cnt5 = 0; }
-        if(str == ""){
-            cout << i << '\n';
-        }
-        else {
-            cout << str << '\n';
-        }
+        cout << (i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : to_string(i)) << '\n';
     }
-
     return 0;
 }
