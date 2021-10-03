79144c4941c826f9569f11791e5a7417063e81ff
Muhammad Raihan Satrio Putra Pamungkas <62191088+satrio-pamungkas@users.noreply.github.com>
2021-10-03 09:19:50 +0700

    Create basic_fizz_buzz.pas

---
+++ Pascal/basic_fizz_buzz.pas
@@ -0,0 +1,17 @@
+program basic_fizz_buzz(output);
+var
+  i: integer;
+begin
+  for i := 1 to 100 do
+    if i mod 3 = 0 then
+      writeln('Fizz')
+      // Multiple of 3
+    else if i mod 5 = 0 then
+      writeln('Buzz')
+      // Multiple of 5
+    else if i mod 15 = 0 then
+      writeln('FizzBuzz')
+      // Multiple of 3 and 5
+    else
+      writeln(i)
+end.
