e8fc7a289cf09c6aac74407283c3ef4978ff8da3
Danang Estutomoaji <ajidanang9@gmail.com>
2021-10-04 17:28:52 +0700

    added haskell version of fizzbuzz

---
+++ Haskell/fizzbuzz.hs
@@ -0,0 +1,12 @@
+-- Basic FizzBuzz with Haskell
+-- Author: @danangtomo
+
+fb :: Integer -> String
+fb n
+  | mod n 3 == 0 && mod n 5 == 0    = "FizzBuzz"
+  | mod n 3 == 0                    = "Fizz"
+  | mod n 5 == 0                    = "Buzz"
+  | otherwise                       = show n
+
+main = do 
+  putStrLn $ unlines (map fb [1..100])
