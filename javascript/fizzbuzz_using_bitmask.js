1129365649fd1e51a8f0e9694eee021f34b426c4
SauravKumarJhaNITW <64098318+SauravKumarJhaNITW@users.noreply.github.com>
2021-10-13 12:54:37 +0530

    Update fizzbuzz_using_bitmask.js

--- javascript/fizzbuzz_using_bitmask.js
+++ javascript/fizzbuzz_using_bitmask.js
@@ -2,13 +2,16 @@
 //Author: @SauravKumarJhaNITW
 
 var values = [undefined, "Fizz", "Buzz", "FizzBuzz"],
-  m = 810092048,
+  //here m represent mask
+  m = 810092048, //11 00 00 01 00 10 01 00 00 01 10 00 01 00 00 (using predefined masks for numbers 0 to 15)
   c = 0;
 
 for (let i = 1; i <= 100; i++) {
+  //will produce num in range of 0 to 3
   c = m & 3;
 
+  //if c is zero => values[c] is undefined, then i will be printed
+  //else if c is 1, 2 or 3 then respective word at that index will be printed
   console.log(i, values[c] || i);
-
   m = (m >> 2) | (c << 28);
 }
